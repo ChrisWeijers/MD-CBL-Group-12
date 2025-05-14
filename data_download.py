80a8535d9a0d24183a1fe60eba83fa7f67437034
Chris Weijers <c.r.j.weijers@student.tue.nl>
2025-05-14 17:03:26 +0200

    Added all necessary files to data_download.py, added data without estimation to extra_data.py

--- data_download.py
+++ data_download.py
@@ -2,6 +2,12 @@ import gdown
 
 gdown.download('https://drive.google.com/uc?id=1wNtUciDlIv4kTDG4jBiYruKJTg-iCQHT',
                'data/Burglary London (2010 - 2025).csv', quiet=False)
+gdown.download('https://drive.google.com/uc?id=1AE-3gou8YjSegrZmydxuKjbrVxhINWZ8',
+               'data/LSOA_(2011)_to_LSOA_(2021)_to_Local_Authority_District_(2022)_Best_Fit_Lookup_for_EW_(V2).csv',
+               quiet=False)
+gdown.download('https://drive.google.com/uc?id=13YDx8Hs115TmAKxPP70boscR6-_W-3lU',
+               'data/LSOA_(2021)_to_Electoral_Ward_(2024)_to_LAD_(2024)_Best_Fit_Lookup_in_EW.csv',
+               quiet=False)
 gdown.download('https://drive.google.com/uc?id=1Pcjeva3gzo_iR0fgOeP2VKIx3KveOhfl',
                'data/hours_worked_2011.csv', quiet=False)
 gdown.download('https://drive.google.com/uc?id=1Hrf-MTzJ5Hu09y2hpyvr9xu7Wt2wUGfx',
