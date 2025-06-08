be392211f4320de299d02ccd5dbc23bed958e8d6
lmb0123 <l.m.bierman@student.tue.nl>
2025-06-08 12:54:55 +0200

    Made a wards geojson file

--- dashleaflet.py
+++ dashleaflet.py
@@ -8,27 +8,42 @@ import pandas as pd
 from dash import dcc
 import plotly.express as px
 
-geojson_path = 'data/london_lsoa_combined.geojson' #geojson file location
-data_path = 'data/burglary_london.csv' #ofc later change to the path of the predicted stuff
+geojson_path = r'C:\Users\20233284\PycharmProjects\MD-CBL-Group-12\data\london_lsoa_combined.geojson' #geojson file location
+geojson_pathw = r"C:\Users\20233284\Documents\Huiswerk\Data Challenge 2\wards3\Wards_May_2024_Boundaries_UK_BSC_8498175397534686318.geojson" #geojson file location
+data_path = 'data/lsoa_predictions.csv'
+dataw_path = 'data/ward_predictions.csv'
 #Load GeoJSON boundaries
 gdf = gpd.read_file(geojson_path)
+gdfw = gpd.read_file(geojson_pathw)
+#print(gdfw.head().to_string())
+
 #Load burglary data
 df = pd.read_csv(data_path, parse_dates=['Month'], low_memory=False)
+dfw = pd.read_csv(dataw_path, parse_dates=['Month'], low_memory=False)
+
 #Count burglary incidents per LSOA code
-burglary_counts = df.groupby('LSOA code').size().reset_index(name='Count')
-merged = gdf.merge(burglary_counts, left_on="lsoa21cd", right_on="LSOA code", how="left")
-merged['Count'] = merged['Count'].fillna(0)
+burglary_counts = df.groupby('LSOA code 2021').sum(numeric_only=True).drop(columns=['Year']).reset_index()
+merged = gdf.merge(burglary_counts, left_on="lsoa21cd", right_on="LSOA code 2021", how="left")
+merged['Predicted burglary count'] = merged['Predicted burglary count'].fillna(0)
 merged = merged.to_crs(epsg=4326)
 merged_json = json.loads(merged.to_json())
+#Count burglary incidents per ward code
+burglary_counts = dfw.groupby('Ward code 2024').sum(numeric_only=True).drop(columns=['Year']).reset_index()
+mergedw = gdfw.merge(burglary_counts, left_on="WD24CD", right_on="Ward code 2024", how="left")
+mergedw[('Predicted burglary count')] = mergedw['Predicted burglary count'].fillna(0)
+mergedw = mergedw.to_crs(epsg=4326)
+WARDSDF = json.loads(mergedw.to_json())
+WARDSDF['features'] = [ward for ward in WARDSDF['features'] if not ward['properties']['Ward code 2024'] == None]
+wards = [ward['properties']['Ward code 2024'] for ward in WARDSDF['features']]
 
 def get_info(feature=None):
     header = [html.H4("London Burglary Count")]
     if not feature:
-        return header + [html.P("Hoover over a LSOA")]
+        return header + [html.P("Hover over an area")]
     return header + [
-        html.B(feature["properties"]["lsoa21nm"]),
+        html.B(feature["properties"]["WD24CD"]),
         html.Br(),
-        "{:.0f} Burglaries".format(feature["properties"]["Count"]),
+        "{:.0f} Burglaries".format(feature["properties"]["Predicted burglary count"]),
     ]
 
 
@@ -56,12 +71,12 @@ style_handle = assign("""function(feature, context){
 }""")
 # Create geojson.
 geojson = dl.GeoJSON(
-    data=merged_json,  # url to geojson file
+    data=WARDSDF,  # url to geojson file
     style=style_handle,  # how to style each polygon
     zoomToBounds=True,  # when true, zooms to bounds when data changes (e.g. on load)
     zoomToBoundsOnClick=True,  # when true, zooms to bounds of feature (e.g. polygon) on click
     hoverStyle=arrow_function(dict(weight=5, color="black", dashArray="")),  # style applied on hover
-    hideout=dict(colorscale=colorscale, classes=classes, style=style, colorProp="Count"),
+    hideout=dict(colorscale=colorscale, classes=classes, style=style, colorProp="Predicted burglary count"),
     id="geojson",
 )
 # Create info control.
@@ -99,7 +114,7 @@ def update_line_chart(feature, nclicks):
             df_ = df[feature['properties']['lsoa21cd'] == df['LSOA code']]
             df_ = df_.groupby('Month').size().reset_index(name='Burglary Count')
             fig = px.line(df_, x=df_['Month'], y=df_['Burglary Count'], markers=True, labels={'x': "Months", 'y': "Burglaries"}, title='Monthly burglaries of selected LSOA')
-            return fig
+            return fig, 0
         else:
             df_ = df.groupby('Month').size().reset_index(name='Burglary Count')
             fig = px.line(df_, x=df_['Month'], y=df_['Burglary Count'], markers=True, labels={'x': "Months", 'y': "Burglaries"}, title='Monthly burglaries of London')
